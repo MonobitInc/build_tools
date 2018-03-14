7da8fec69ba5af5d0a2c684e649c3687d0ff9e5a
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-03-14 09:10:35 +0900

    r16

--- setup_ndk.sh
+++ setup_ndk.sh
@@ -2,15 +2,17 @@
 
 NDKVER=$1
 
-if [[ $NDKVER = *"r12b"* ]]; then
-    FULLVER=android-ndk-r12b
+if [[ ($NDKVER == "r12b" ) || ($NDKVER == "r16b") ]]; then
+    echo setup ndk version: ${NDKVER}
+    FULLVER=android-ndk-${NDKVER}
     ZIPFILE=${FULLVER}-linux-x86_64.zip
     URL=https://dl.google.com/android/repository/${ZIPFILE}
     wget --quiet ${URL}
     unzip -q ${ZIPFILE}
 fi
 
-if [[ $NDKVER = *"r10e"* ]]; then
+if [[ $NDKVER == "r10e" ]]; then
+    echo setup ndk version: ${NDKVER}    
     FULLVER=android-ndk-r10e
     URL=http://dl.google.com/android/ndk/${FULLVER}-linux-x86_64.bin
     wget --quiet ${URL}
